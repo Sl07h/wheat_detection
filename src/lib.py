ca6fa0cb228be59e4c4db009a121fce0b6aee1de
Sl07h <43113519+Sl07h@users.noreply.github.com>
2021-11-01 18:10:27 +0700

    fixed typo in src/lib.py

--- src/lib.py
+++ src/lib.py
@@ -212,7 +212,7 @@ def handle_metadata(filenames, path_field_day):
             for filename in filenames:
                 path_img = path_field_day + 'src/' + filename
                 path_csv = path_field_day + 'tmp/' + filename[:-4] + '.csv'
-                command = 'exiftool-12.34\exiftool -csv {} > {}'.format(path_img, path_csv)
+                command = 'exiftool-12.34/exiftool -csv {} > {}'.format(path_img, path_csv)
                 os.system(command)
                 df = pd.read_csv(path_csv, header=None).T
                 df.to_csv(path_csv, header=False, index=False)
