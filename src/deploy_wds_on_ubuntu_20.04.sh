bcecdc41828515866ca48f72e336368b3f91d268
Sl07h <43113519+Sl07h@users.noreply.github.com>
2022-02-01 15:18:35 +0700

    added deployment script and instruction in russian

---
+++ src/deploy_wds_on_ubuntu_20.04.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt install git
+sudo apt install p7zip-full
+sudo apt install python3-pip
+sudo apt install python3-venv
+
+git clone https://github.com/Sl07h/wheat_detection/
+cd wheat_detection
+. src/install.sh
