fdc0b2ae2ec00aed9de21f5a54242f532a89edb4
Sl07h <43113519+Sl07h@users.noreply.github.com>
2022-02-05 23:16:19 +0700

    fixed pycocotools build

--- src/deploy_wds_on_windows_10.ps1
+++ src/deploy_wds_on_windows_10.ps1
@@ -2,40 +2,45 @@
 
 Function DownloadFromGoogleDrive ([String]$URL)
 {
-    (Invoke-WebRequest -Uri $URL -SessionVariable googleDriveSession).Links.Href > qwe.txt
+    Invoke-WebRequest -Uri $URL -OutFile qwe.txt -SessionVariable googleDriveSession
     $text = Get-Content .\qwe.txt -Raw 
     $pattern="confirm=(\w\w\w\w)"
     $text -match $pattern
     $token = $matches[0]
     $DownloadURL = -join($URL, '&', $token)
     $DownloadURL
-    Invoke-WebRequest -Uri $DownloadURL -OutFile "arch.zip" -WebSession $googleDriveSession
+    $res = Invoke-WebRequest -Uri $DownloadURL -OutFile "arch.zip" -WebSession $googleDriveSession
+    $res
     7z x arch.zip
     rm arch.zip
     rm qwe.txt
 }
 
+# 1.python и pip
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.8.10/python-3.8.10-amd64.exe" -OutFile "python-3.8.10-amd64.exe"
+# https://stackoverflow.com/questions/1741490/how-to-tell-powershell-to-wait-for-each-command-to-end-before-starting-the-next
+./python-3.8.10-amd64.exe /quiet `
+                        PrependPath=1 `
+                        InstallAllUsers=0 `
+                        Include_launcher=0 `
+                        Include_doc=0 `
+                        Include_tcltk=0 `
+                        Include_test=0 | Out-Null
+rm python-3.8.10-amd64.exe
+$env:Path += ";" + $env:LOCALAPPDATA + "\Programs\Python\Python38"
 
-# 1. 7zip
+# 2. 7zip
 Invoke-WebRequest -Uri "https://www.7-zip.org/a/7z2107-x64.exe" -OutFile "7z2107-x64.exe"
-Start-Process -FilePath 7z2107-x64.exe -Args "/S" -Verb RunAs -Wait
+./7z2107-x64.exe /S
 $env:Path += ";C:\Program Files\7-Zip"
 rm 7z2107-x64.exe
 
-# 2. Сама система
+# 3. Сама система
 Invoke-WebRequest -Uri "https://github.com/Sl07h/wheat_detection/archive/refs/heads/master.zip" -OutFile "master.zip"
 7z x master.zip
 mv wheat_detection-master wheat_detection
 rm master.zip
 cd wheat_detection
-
-# 3.python и pip
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.8.10/python-3.8.10-amd64.exe" -OutFile "python-3.8.10-amd64.exe"
-# https://stackoverflow.com/questions/1741490/how-to-tell-powershell-to-wait-for-each-command-to-end-before-starting-the-next
-./python-3.8.10-amd64.exe /quiet PrependPath=1 InstallAllUsers=0 InstallLauncherAllUsers=0 Include_launcher=0 Include_doc=0 Include_tcltk=0 Include_test=0  | Out-Null
-rm python-3.8.10-amd64.exe
-# refresh path to see python from current shell
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";C:\Program Files\7-Zip;" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 # 4.exiftool
 Invoke-WebRequest -Uri "https://exiftool.org/exiftool-12.34.zip" -OutFile "exiftool-12.34.zip"
@@ -50,8 +55,15 @@ python -m venv wds_venv
 # 6. активируем её
 wds_venv/Scripts/Activate.ps1
 
-# 7. ставим библиотеки питона
+# 7. ставим библиотеки питона и MS build tools, т.к. без них не работет pycocotools
+Invoke-WebRequest -Uri "https://aka.ms/vs/17/release/vs_buildtools.exe" -OutFile "vs_buildtools.exe"
+$proc = Start-Process `
+    -FilePath vs_buildtools.exe `
+    -ArgumentList "--quiet",  "--wait", "--add", "Microsoft.VisualStudio.Workload.VCTools" `
+    -Wait -PassThru
+$returnCode = $proc.ExitCode
 pip install -r requirements.txt
+rm vs_buildtools.exe
 
 
 # 8. качаем веса нейросетей
